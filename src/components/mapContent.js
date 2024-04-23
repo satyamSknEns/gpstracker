3a2b02fa092dea9a204fc43f1737effe57ff589c
satyamn96 <satyamskn96@gmail.com>
2024-04-23 12:08:13 +0530

    changes

--- src/components/mapContent.js
+++ src/components/mapContent.js
@@ -6,55 +6,57 @@ const MapComponent = () => {
     let allData = JSON.parse(window.localStorage.getItem("coordinates"));
     console.log("allData", allData);
     let centeredElement;
+    if (allData) {
+      if (allData.length % 2 === 0) {
+        const centerIndex = allData.length / 2;
+        centeredElement = allData[centerIndex];
+      } else {
+        const centerIndex = Math.floor(allData.length / 2);
+        centeredElement = allData[centerIndex];
+      }
 
-    if (allData.length % 2 === 0) {
-      const centerIndex = allData.length / 2;
-      centeredElement = allData[centerIndex];
-    } else {
-      const centerIndex = Math.floor(allData.length / 2);
-      centeredElement = allData[centerIndex];
-    }
+      console.log("Centered element:", centeredElement);
 
-    console.log("Centered element:", centeredElement);
-    mapboxgl.accessToken =
-      "pk.eyJ1IjoiZW5zZW50ZXJwcmlzZXMiLCJhIjoiY2x2MjQ5NWprMGFqNjJscDhzeGZkcm5yayJ9.WloQmfc86sJgpQOQ8W9RHg";
-    const map = new mapboxgl.Map({
-      container: "map",
-      style: "mapbox://styles/mapbox/streets-v12",
-      //   projection:"globe",
-      //   projection:"naturalEarth",
-      center: centeredElement,
-      zoom: 15,
-    });
-    map.on("load", () => {
-      map.addSource("route", {
-        type: "geojson",
-        data: {
-          type: "Feature",
-          properties: {},
-          geometry: {
-            type: "LineString",
-            coordinates: allData,
+      mapboxgl.accessToken =
+        "pk.eyJ1IjoiZW5zZW50ZXJwcmlzZXMiLCJhIjoiY2x2MjQ5NWprMGFqNjJscDhzeGZkcm5yayJ9.WloQmfc86sJgpQOQ8W9RHg";
+      const map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v12",
+        //   projection:"globe",
+        //   projection:"naturalEarth",
+        center: centeredElement,
+        zoom: 15,
+      });
+      map.on("load", () => {
+        map.addSource("route", {
+          type: "geojson",
+          data: {
+            type: "Feature",
+            properties: {},
+            geometry: {
+              type: "LineString",
+              coordinates: allData,
+            },
           },
-        },
+        });
+
+        map.addLayer({
+          id: "route",
+          type: "line",
+          source: "route",
+          layout: {
+            "line-join": "round",
+            "line-cap": "round",
+          },
+          paint: {
+            "line-color": "#888",
+            "line-width": 8,
+          },
+        });
       });
 
-      map.addLayer({
-        id: "route",
-        type: "line",
-        source: "route",
-        layout: {
-          "line-join": "round",
-          "line-cap": "round",
-        },
-        paint: {
-          "line-color": "#888",
-          "line-width": 8,
-        },
-      });
-    });
-
-    return () => map.remove();
+      return () => map.remove();
+    }
   }, []);
 
   return (
